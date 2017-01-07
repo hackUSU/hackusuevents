136921274b75e554501d9222da562fef7e8b334e
Haley Manning <haleyrmanning@gmail.com>
2017-01-06 22:30:09 -0700

    added the events dropdown menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#events").click(function(){
+    $(".events-dropdown-hide, .events-dropdown-show").toggleClass("events-dropdown-hide events-dropdown-show");
+  });
+})
